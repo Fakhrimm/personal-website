15ce3243ab389085cc2c37fbd8f169373438addc
Fakhrimm <89113487+Fakhrimm@users.noreply.github.com>
2025-09-02 11:01:17 +0700

    feat: init

---
+++ tailwind.config.ts
@@ -0,0 +1,23 @@
+// import type { Config } from 'tailwindcss';
+
+// const config: Config = {
+//   content: ['./src/**/*.{html,js,ts,jsx,tsx}', './index.html', './**/*.html'],
+//   theme: {
+//     extend: {
+//       colors: {
+//         brand: '#3B82F6',
+//         accent: '#10B981',
+//         'brand-blue': {
+//           50: '#EFF6FF',
+//           100: '#DBEAFE',
+//           500: '#3B82F6',
+//           600: '#2563EB',
+//           900: '#1E3A8A',
+//         },
+//       },
+//     },
+//   },
+//   plugins: [require('daisyui')],
+// };
+
+// export default config;
